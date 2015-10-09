2f302da051902c5fd29076329baa9f4265b7db9a
lfepp <lucasfepp@gmail.com>
2015-10-09 17:45:55 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var root = 'https://congress.api.sunlightfoundation.com';
+var apiKey = 'c5be18657d7e405d886fd840845aa279';
+//HAVENT FINISHED FIELDS
+var fields = '?fields=';
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/views/index.html');
+})
+
+app.listen(9000);
+module.exports = app;
