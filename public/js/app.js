7f2eb77b2e48fec474fb59de2aafb521b246987f
lfepp <lucasfepp@gmail.com>
2015-10-10 12:27:30 -0400

    Updated rep headers to panels

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@ var data = JSON.parse(passedData.substring(35));
 // Loop to cycle through each representative
 for (var rep in data.results) {
   // Display rep name and party
-  $('.container').append('<div class="page-header"><h2>' + data.results[rep].first_name + ' ' + data.results[rep].last_name + ' <small>' + data.results[rep].party + ' - ' + data.results[rep].state + '</small></h2></div>');
+  $('.container').append('<div class="panel panel-default"><div class="panel-heading"><h2 class="panel-title">' + data.results[rep].first_name + ' ' + data.results[rep].last_name + ' </h2>' + data.results[rep].party + ' - ' + data.results[rep].state + '</div></div>');
   // Display rep chamber
   // Display rep phone
   // Display rep email
