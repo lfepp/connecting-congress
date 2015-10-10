77d484bbb0ce3f27a731d712fd517719105f0f70
lfepp <lucasfepp@gmail.com>
2015-10-10 10:29:42 -0400

    Finished templating for API queries

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 'use strict';
 
-var root = 'https://congress.api.sunlightfoundation.com';
-var apiKey = 'c5be18657d7e405d886fd840845aa279';
-//HAVENT FINISHED FIELDS
-var fields = '&fields=';
+// Query to call Sunlight Congress API
+var root = 'https://congress.api.sunlightfoundation.com/legislators/locate?zip=';
+var apiKey = '&apikey=c5be18657d7e405d886fd840845aa279';
+var fields = '&fields=chamber,first_name,last_name,party,phone,oc_email,contact_form,facebook_id,twitter_id,youtube_id,website';
+var zipCode = '';
+var query = root + zipCode + apiKey + fields;
 
 var express = require('express');
 var app = express();
