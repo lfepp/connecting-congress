7c2e3cc553a8bbdae99329590937eccf411b3fd6
lfepp <lucasfepp@gmail.com>
2015-10-10 12:22:21 -0400

    Added header for each rep

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,14 @@
 var passedData = decodeURIComponent(window.location.href);
 var data = JSON.parse(passedData.substring(35));
-console.dir(data);
-console.log(typeof data);
+
+// Loop to cycle through each representative
+for (var rep in data.results) {
+  // Display rep name and party
+  $('.container').append('<div class="page-header"><h2>' + data.results[rep].first_name + ' ' + data.results[rep].last_name + ' <small>' + data.results[rep].party + ' - ' + data.results[rep].state + '</small></h2></div>');
+  // Display rep chamber
+  // Display rep phone
+  // Display rep email
+  // Display rep website
+  // Display/embed rep contact form
+  // Display rep social channels (FB, Twitter, YT)
+}
