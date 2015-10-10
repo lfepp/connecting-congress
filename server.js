907b7f282daf20508f1a13d8d287a08dc334371e
lfepp <lucasfepp@gmail.com>
2015-10-10 11:16:07 -0400

    Parse form data from index.html into query for Sunlight Congress API

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 'use strict';
 
 // Query to call Sunlight Congress API
-var root = 'https://congress.api.sunlightfoundation.com/legislators/locate?zip=';
+var root = 'https://congress.api.sunlightfoundation.com/legislators/locate?';
 var apiKey = '&apikey=c5be18657d7e405d886fd840845aa279';
 var fields = '&fields=chamber,first_name,last_name,party,phone,oc_email,contact_form,facebook_id,twitter_id,youtube_id,website';
 var zipCode = '';
-var query = root + zipCode + apiKey + fields;
 
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
+app.use(bodyParser.text());
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/views/index.html');
@@ -20,7 +19,9 @@ app.get('/', function(req, res) {
 
 app.post('/api', function(req, res) {
   res.sendFile(__dirname + '/public/views/contact.html');
-  console.dir(req.body);
+  zipCode = req.body;
+  var query = root + zipCode + apiKey + fields;
+  console.log(query);
 })
 
 app.listen(9000);
