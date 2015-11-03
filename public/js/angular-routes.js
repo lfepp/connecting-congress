db5e70377a7f346e493a31c715876811b0ff21b2
lfepp <lucasfepp@gmail.com>
2015-11-02 20:34:47 -0500

    Pulled in search as first view

---
+++ public/js/angular-routes.js
@@ -0,0 +1,11 @@
+angular.module('angularRoutes', []).config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+$urlRouterProvider.otherwise('/');
+
+$stateProvider
+  .state('home', {
+    url: '/',
+    templateUrl: 'views/home.html'
+  })
+
+}]);
