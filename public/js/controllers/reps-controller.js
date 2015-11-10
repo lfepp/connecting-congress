1b3c804a98bfbb013ddc39919f806dd353eb2e14
Luke Epp <lucasfepp@gmail.com>
2015-11-10 17:32:56 -0500

    Dealt with passing data to reps-controller and added get data function to main-service

--- public/js/controllers/reps-controller.js
+++ public/js/controllers/reps-controller.js
@@ -1,3 +1,5 @@
 angular.module('RepsCtrl', ['MainService']).controller('RepsController', ['$scope', 'Reps', function($scope, Reps) {
-
+  $('#error-alert').hide();
+  $scope.reps = Reps.get();
+  console.dir($scope.reps);
 }]);
--- public/js/services/main-service.js
+++ public/js/services/main-service.js
@@ -1,28 +1,29 @@
 angular.module('MainService', []).factory('Reps', ['$http', function($http) {
+  var responseData = {};
 
   return {
     search: function(zipCode) {
-      return $http({
+      $http({
         method: 'POST',
         url: '/api',
         params: {
           zip: zipCode
         }
       })
-      // JUST MOVED THIS DATA, NEED TO CLEAN AND REMOVE $SCOPE, THIS IS A SERVICE
       .then(function successResponse(response) {
-        var responseData = JSON.parse(response.data.body);
-        if(responseData.count < 1) {
+        if(JSON.parse(response.data.body).count < 1) {
           console.error('ERROR: INVALID ZIP CODE');
-          $('#error-alert').show();
         } else {
           console.log('Success');
-          $scope.reps = responseData;
+          responseData = JSON.parse(response.data.body);
         }
       },
       function failureResponse(response) {
         console.log('Error ' + response);
       });
+    },
+    get: function() {
+      return responseData;
     }
   }
 }])
