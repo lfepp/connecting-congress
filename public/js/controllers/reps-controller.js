3ab5157fb0296d380034c40477fe6cc4992f7f55
Luke Epp <lucasfepp@gmail.com>
2015-11-10 21:50:27 -0500

    Set up ngRepeat

--- public/js/controllers/reps-controller.js
+++ public/js/controllers/reps-controller.js
@@ -1,7 +1,7 @@
 angular.module('RepsCtrl', ['MainService']).controller('RepsController', ['$scope', 'Reps', function($scope, Reps) {
   $('#error-alert').hide();
   $scope.$on('dataCollected', function(event, responseData) {
-    $scope.reps = responseData;
+    $scope.reps = responseData.responseData;
     console.dir($scope.reps);
   })
 }]);
