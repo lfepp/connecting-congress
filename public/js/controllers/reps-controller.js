d90340960d8b38bbfc0e5b815f336263986676cf
lfepp <lucasfepp@gmail.com>
2016-03-26 23:42:32 -0400

    Added footer, icon, and style changes

--- public/js/controllers/reps-controller.js
+++ public/js/controllers/reps-controller.js
@@ -4,7 +4,8 @@ angular.module('RepsCtrl', ['MainService']).controller('RepsController', ['$scop
     $scope.reps = responseData.responseData;
     for(var i=0; i<$scope.reps.results.length; i++) {
       $scope.reps.results[i].properChamber = $scope.reps.results[i].chamber[0].toUpperCase() + $scope.reps.results[i].chamber.substring(1);
+      angular.element(document.querySelector('#footer')).css({ 'margin-top': '+=280px' });
     }
-    console.dir($scope.reps);
+    console.dir(angular.element(document.querySelector('#footer')));
   })
 }]);
