5a612bfb5db8e3fbe20430f99189dbb9c1653471
Luke Epp <luke@formswift.com>
2017-10-01 15:43:01 -0700

    Added config file for api keys

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 'use strict';
 
-// Query to call Sunlight Congress API
-var root = 'https://congress.api.sunlightfoundation.com/legislators/locate?zip=';
-var zipCode = '';
-var apiKey = '&apikey=c5be18657d7e405d886fd840845aa279';
-var fields = '&fields=bioguide_id,chamber,first_name,last_name,party,phone,oc_email,facebook_id,twitter_id,youtube_id,website,state';
+// // Query to call Sunlight Congress API
+// var root = 'https://congress.api.sunlightfoundation.com/legislators/locate?zip=';
+// var zipCode = '';
+// var apiKey = '&apikey=c5be18657d7e405d886fd840845aa279';
+// var fields = '&fields=bioguide_id,chamber,first_name,last_name,party,phone,oc_email,facebook_id,twitter_id,youtube_id,website,state';
 
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
+
+var config = JSON.parse(__dirname + 'config.json');
+
+var googleCivicKey = config.google.key;
+var googleCivicOptions = {
+  url: '',
+};
+
+var proPublicaKey = config.propublica.key;
+var proPublicaOptions = {
+  url: '',
+  headers: {},
+};
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.text());
