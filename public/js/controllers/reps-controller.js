31068e94d22a110f1cbee7079a283038fe3d1c98
Luke Epp <luke@formswift.com>
2017-10-04 13:29:54 -0700

    WIP: Parsed Google Civic API data for front-end

--- public/js/controllers/reps-controller.js
+++ public/js/controllers/reps-controller.js
@@ -1,9 +1,10 @@
 angular.module('RepsCtrl', ['MainService']).controller('RepsController', ['$scope', 'Reps', function($scope, Reps) {
   $('#error-alert').hide();
   $scope.$on('dataCollected', function(event, responseData) {
-    $scope.reps = responseData.responseData;
-    for(var i=0; i<$scope.reps.results.length; i++) {
-      $scope.reps.results[i].properChamber = $scope.reps.results[i].chamber[0].toUpperCase() + $scope.reps.results[i].chamber.substring(1);
+    $scope.reps = responseData.responseData.officials;
+    for(var i=0; i<$scope.reps.length; i++) {
+      // $scope.reps.results[i].properChamber = $scope.reps.results[i].chamber[0].toUpperCase() + $scope.reps.results[i].chamber.substring(1);
+      $scope.reps.
       angular.element(document.querySelector('#footer')).css({ 'margin-top': '+=280px' });
     }
     console.dir(angular.element(document.querySelector('#footer')));
