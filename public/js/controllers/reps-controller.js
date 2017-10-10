d95f3c7977408c9c86074259d220f12c396934ee
Luke Epp <luke@formswift.com>
2017-10-09 22:00:28 -0700

    Added controller to include information from new API

--- public/js/controllers/reps-controller.js
+++ public/js/controllers/reps-controller.js
@@ -2,11 +2,22 @@ angular.module('RepsCtrl', ['MainService']).controller('RepsController', ['$scop
   $('#error-alert').hide();
   $scope.$on('dataCollected', function(event, responseData) {
     $scope.reps = responseData.responseData.officials;
-    for(var i=0; i<$scope.reps.length; i++) {
-      // $scope.reps.results[i].properChamber = $scope.reps.results[i].chamber[0].toUpperCase() + $scope.reps.results[i].chamber.substring(1);
-      $scope.reps.
-      angular.element(document.querySelector('#footer')).css({ 'margin-top': '+=280px' });
+    for(var i = 0; i < $scope.reps.length; i += 1) {
+      for(var j = 0; j < $scope.reps[i].channels.length; j += 1) {
+        switch ($scope.reps[i].channels[j].type) {
+          case 'Facebook':
+            $scope.reps[i].facebook_id = $scope.reps[i].channels[j].id;
+            break;
+          case 'Twitter':
+            $scope.reps[i].twitter_id = $scope.reps[i].channels[j].id;
+            break;
+          case 'YouTube':
+            $scope.reps[i].youtube_id = $scope.reps[i].channels[j].id;
+            break;
+          default:
+            break;
+        }
+      }
     }
-    console.dir(angular.element(document.querySelector('#footer')));
-  })
+  });
 }]);
